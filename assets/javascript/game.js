a106f0a29878ed89320289080d16e8ff87424bef
Jayrene1 <43413926+Jayrene1@users.noreply.github.com>
2018-12-03 13:55:41 -0500

    added some unimplemented id tags to index.html, and sketched more layout with text. Added a few more functions to game.js to compare user input to game word, as well as end the game/reset the game.

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -2,10 +2,10 @@
 var guessesLeft;
 var wins = 0;
 var losses = 0;
-var allLetters = ["a", "b" "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+var allLetters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 
 // game theme using list of words
-var words: ["jimi hendrix", "eddie van halen", "eric clapton", "slash", "george harrison", "brian may", "john mayer", "steve vai", "prince", "jack white"];
+var words = ["jimi hendrix", "eddie van halen", "eric clapton", "slash", "george harrison", "brian may", "john mayer", "steve vai", "prince", "jack white"];
 
 // game object
 var game = {
@@ -14,17 +14,93 @@ var game = {
     lettersCorrect: [],
     lettersWrong: [],
     lettersRemaining: [],
+    lettersBlank: 0,
+    lettersBlankAndCorect: [],
 
 // game functions
-    initializeGame: function () {
-        this.randomWord = "";
+// resets the game
+    Initialize: function () {
+        this.randomWord;
         this.randomWordSplit = [];
+        this.lettersBlankAndCorect = [];
         this.lettersCorrect = [];
         this.lettersWrong = [];
         this.lettersRemaining = allLetters;
+        this.lettersBlank = 0;
         guessesLeft = 6;
+        console.log("Game Initialized");
+    },
+// chooses a random word to guess
+    chooseWord: function () {
+        this.randomWord = words[Math.floor(Math.random() * words.length)];
+        console.log("Random word chosen: " + this.randomWord);
+
+        this.randomWordSplit = this.randomWord.split("");
+        console.log("Random word split: " + this.randomWordSplit);
+
+        this.lettersBlank = this.randomWordSplit.length;
+        console.log("Letter Length: " + this.lettersBlank);
+
+        for (var i = 0; i < this.lettersBlank; i++) {
+            this.lettersBlankAndCorect.push("_");
+            document.getElementById("randomWord").innerHTML = " " + this.lettersBlankAndCorect.join(" ");
+
+        }
+        console.log("Letters Blank and Correct: " + this.lettersBlankAndCorect);
+    },
+
+// passes in user input to check against random word
+    checkGuess: function(guess) {
+        console.log("guess: " + guess);
+        var goodGuess = false;
+
+        for (var i = 0; i < this.lettersBlank; i++) {
+            if (this.randomWord[i] == guess) {
+                goodGuess = true;
+            }
+        }
+        console.log("goodGuess: " + goodGuess);
+
+        if (goodGuess) {
+            for (var i = 0; i < this.lettersBlank; i++) {
+                if (this.randomWord[i] == guess) {
+                    this.lettersBlankAndCorect[i] = guess;
+                }
+            }
+        } else{
+            this.lettersWrong.push(guess);
+            guessesLeft--;
+        }
+        console.log("Letters Blank and Correct: " + this.lettersBlankAndCorect);
+        console.log("Letters wrong: " + this.lettersWrong);
+        console.log("Guesses remaining: " + guessesLeft);
+    },
+
+// runs to check if the game has ended, and restarts game if true
+    gameOver: function() {
+        if (this.randomWordSplit.toString() == this.lettersBlankAndCorect.toString()) {
+            wins++;
+            this.Initialize();
+            this.chooseWord();
+            console.log("| Wins: " + wins + " Losses: " + losses + " |")
+            return true;
+        } else if (guessesLeft == 0) {
+            losses++;
+            this.Initialize();
+            this.chooseWord();
+            console.log("| Wins: " + wins + " Losses: " + losses + " |")
+            return true;
+        } else {
+            return false;
+        }
     }
+}
 
-
-
+// calls to start game
+game.Initialize();
+game.chooseWord();
+document.onkeyup = function (event) {
+    var guess = String.fromCharCode(event.keyCode).toLowerCase();
+    game.checkGuess(guess);
+    game.gameOver();
 }
